ce89fc768c86fbe8042d08f72b7fc30c024208d0
RISHI PAWAR <161636122+rishipawar0025@users.noreply.github.com>
2026-07-22 00:28:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
     model_name="llama-3.3-70b-versatile",
     temperature=0.2
 )
